0a76d8e6eedbd6c53e4fbc8b8ba0bb4ea919bc55
AZysk0 <arko.zystem@gmail.com>
2021-06-04 06:59:02 +0300

    asd

--- myblog/tests.py
+++ myblog/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
-
+from django.test import Client
 
 # Create your tests here.
 class Test(TestCase):
-    pass
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_details(self):
+        response = self.client.get('/customer/details/')
+        self.assertEqual(response.status_code, 200)
